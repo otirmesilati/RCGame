3b30a186775ef49fd186103054b38e2d8d7d7d66
Otir <45825931+otirmesilati@users.noreply.github.com>
2023-03-28 19:20:32 +0300

    Adding the base mechanics of the player's movement

--- game_settings.py
+++ game_settings.py
@@ -1,3 +1,8 @@
 # User interface changeable settings for the game
 RESOLUTION = WIDTH, HEIGHT = 800, 600
 FRAMES = 60
+
+PLAYER_POSITION = 1.5, 5
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
