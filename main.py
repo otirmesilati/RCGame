4b7133f465e2c327dc963baa92a1c04d438390ec
Otir <45825931+otirmesilati@users.noreply.github.com>
2023-03-30 14:03:45 +0300

    Introducing sprites
    Currently see-through walls, will be
    addressed in the next patch!

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from game_map import *
 from character import *
 from raycast import *
 from object_renderer import *
+from sprite_obj import *
 
 
 class Game:
@@ -21,10 +22,12 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
+        self.static_sprite = SpriteObject(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
+        self.static_sprite.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FRAMES)
         pg.display.set_caption(f'{self.clock.get_fps(): .1f}')
