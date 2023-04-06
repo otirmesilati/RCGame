c68e117d8eeb0f68ed8768e6d4bf2e4acaf3a6b1
Otir <45825931+otirmesilati@users.noreply.github.com>
2023-04-06 11:44:17 +0300

    NPC class update
    Adding the images for the NPC movement

--- npc.py
+++ npc.py
@@ -3,4 +3,14 @@ from random import randint, random, choice
 
 
 class NPC(Animated_sprite):
-    pass
+    def __init__(self, game, path='resources/sprites/npc/soldier/0.png', pos=(10.5, 5.5),
+                 scale=0.6, shift=0.38, animation_time=180):
+        super().__init__(game, path, pos, scale, shift, animation_time)
+        self.attack_images = self.get_images(self.path + '/attack')
+        self.death_images = self.get_images(self.path + '/death')
+        self.idle_images = self.get_images(self.path + '/idle')
+        self.pain_images = self.get_images(self.path + '/pain')
+        self.walk_images = self.get_images(self.path + '/walk')
+
+
+
