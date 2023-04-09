8cc979166226ddc35f9a331aeaa0f24bf0121272
Otir <45825931+otirmesilati@users.noreply.github.com>
2023-04-09 08:34:07 +0300

    NPC patching
    Updating "when-hit" logic

--- npc.py
+++ npc.py
@@ -26,7 +26,22 @@ class NPC(Animated_sprite):
         self.get_sprite()
         self.run_logic()
 
+    def animate_pain(self):
+        self.animate(self.pain_images)
+        if self.animation_trigger:
+            self.pain = False
+
+    def check_hit_npc(self):
+        if self.game.player.shot:
+            if HALF_WIDTH - self.sprite_half_width < self.screen_x < HALF_WIDTH + self.sprite_half_width:
+                self.game.player.shot = False
+                self.pain = True
+
     def run_logic(self):
         if self.alive:
-            self.animate(self.idle_images)
+            self.check_hit_npc()
+            if self.pain:
+                self.animate_pain()
+            else:
+                self.animate(self.idle_images)
 
